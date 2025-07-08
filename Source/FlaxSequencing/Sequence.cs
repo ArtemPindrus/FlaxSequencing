576a97fd44b6c074c2d23de776ab9b02b578be38
Artem Pindrus <artempindrus@gmail.com>
2025-07-08 16:31:32 +0300

    cleanup

--- Source/FlaxSequencing/Sequence.cs
+++ Source/FlaxSequencing/Sequence.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using FlaxEngine;
-using static FlaxEditor.GUI.ItemsListContextMenu;
 
 namespace Sequencing;
 
